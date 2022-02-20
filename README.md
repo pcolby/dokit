e9084336d7d8e98167fd9e676234cf361fcfec63
Paul Colby <git@colby.id.au>
2022-02-20 17:39:56 +1100

    Add initial project layout

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 # Poqit
 
-Poqit is a cross-platform desktop GUI for [Pokit] measuring devices
+Poqit is a [Qt] library, and cross-platform desktop apps (both GUI and console) for [Pokit] measuring
+devices.
 
 This project is in very early stages of development, but aims to support both [Pokit Meter] and
 [Pokit Pro], on Linux, Mac and Windows.
+
+*To do: some notes on device x feature x platform x app support matrix.*
 
 
 [Pokit]:       https://www.pokitinnovations.com/ "Pokit Innovations"
 [Pokit Meter]: https://www.pokitinnovations.com/pokit-meter/
 [Pokit Pro]:   https://www.pokitinnovations.com/pokit-pro/
+[Qt]:          https://www.qt.io/
---
+++ src/console/main.cpp
@@ -0,0 +1,91 @@
+/*
+    Copyright 2022 Paul Colby
+
+    This file is part of Poqit.
+
+    Poqit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Poqit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with Poqit.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <QCommandLineParser>
+#include <QCoreApplication>
+#include <QDebug>
+#include <QDir>
+#include <QFileInfo>
+#include <QLoggingCategory>
+
+#if defined(Q_OS_UNIX)
+#include <unistd.h>
+#elif defined(Q_OS_WIN)
+#include <Windows.h>
+#endif
+
+inline bool haveConsole()
+{
+    #if defined(Q_OS_UNIX)
+    return isatty(STDERR_FILENO);
+    #elif defined(Q_OS_WIN)
+    return GetConsoleWindow();
+    #else
+    return false;
+    #endif
+}
+
+void configureLogging(const QCommandLineParser &parser)
+{
+    // Start with the Qt default message pattern (see qtbase:::qlogging.cpp:defaultPattern)
+    QString messagePattern = QStringLiteral("%{if-category}%{category}: %{endif}%{message}");
+
+    if (parser.isSet(QStringLiteral("debug"))) {
+        messagePattern.prepend(QStringLiteral("%{time process} %{type} %{function} "));
+        QLoggingCategory::defaultCategory()->setEnabled(QtDebugMsg, true);
+    }
+
+    const QString color = parser.value(QStringLiteral("color"));
+    if ((color == QStringLiteral("yes")) || (color == QStringLiteral("auto") && haveConsole())) {
+        messagePattern.prepend(QStringLiteral(
+        "%{if-debug}\x1b[37m%{endif}"      // White
+        "%{if-info}\x1b[32m%{endif}"       // Green
+        "%{if-warning}\x1b[35m%{endif}"    // Magenta
+        "%{if-critical}\x1b[31m%{endif}"   // Red
+        "%{if-fatal}\x1b[31;1m%{endif}")); // Red and bold
+        messagePattern.append(QStringLiteral("\x1b[0m")); // Reset.
+    }
+
+    qSetMessagePattern(messagePattern);
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+    app.setApplicationName(QStringLiteral(CMAKE_PROJECT_NAME));
+    app.setApplicationVersion(QStringLiteral(CMAKE_PROJECT_VERSION));
+
+    // Parse the command line options.
+    QCommandLineParser parser;
+    parser.setApplicationDescription(QStringLiteral(CMAKE_PROJECT_DESCRIPTION));
+    parser.addOptions({
+        {{QStringLiteral("d"), QStringLiteral("debug")}, QStringLiteral("Enable debug output")},
+        { QStringLiteral("color"),
+          QCoreApplication::translate("main","Color the console output (default auto)"),
+          QStringLiteral("yes|no|auto"), QStringLiteral("auto")},
+    });
+    parser.addHelpOption();
+    parser.addVersionOption();
+    parser.process(app);
+    configureLogging(parser);
+
+    /// \todo
+
+    return 0;
+}
