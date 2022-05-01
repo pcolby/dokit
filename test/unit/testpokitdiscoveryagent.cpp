b5308b75df7e5a7b011cdfb05d2f2fc291d69f74
Paul Colby <git@colby.id.au>
2022-05-01 10:40:07 +1000

    Implement some basic tests

--- test/unit/testpokitdiscoveryagent.cpp
+++ test/unit/testpokitdiscoveryagent.cpp
@@ -19,14 +19,43 @@
 
 #include "testpokitdiscoveryagent.h"
 
+#include <qtpokit/pokitdiscoveryagent.h>
+
+#include <QBluetoothUuid>
+
+#define POKIT_STATUS_SERVICE_UUID QLatin1String("57d3a771-267c-4394-8872-78223e92aec5")
+
 void TestPokitDiscoveryAgent::isPokitDevice_data()
 {
-    /// \todo
+    QTest::addColumn<QBluetoothDeviceInfo>("info");
+    QTest::addColumn<bool>("expected");
+
+    QTest::addRow("invalid") << QBluetoothDeviceInfo() << false;
+
+    QBluetoothDeviceInfo info(QBluetoothAddress(QLatin1String("11:22:33:44:55:66")), QLatin1String("info"), 0);
+    QTest::addRow("unset") << info << false;
+
+    info.setServiceUuids({});
+    QTest::addRow("empty") << info << false;
+
+    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid())});
+    QTest::addRow("random") << info << false;
+
+    info.setServiceUuids({ QBluetoothUuid(POKIT_STATUS_SERVICE_UUID) });
+    QTest::addRow("status") << info << true;
+
+    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid()), QBluetoothUuid(QUuid::createUuid())});
+    QTest::addRow("two") << info << false;
+
+    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid()), QBluetoothUuid(POKIT_STATUS_SERVICE_UUID)});
+    QTest::addRow("both") << info << true;
 }
 
 void TestPokitDiscoveryAgent::isPokitDevice()
 {
-    /// \todo
+    QFETCH(QBluetoothDeviceInfo, info);
+    QFETCH(bool, expected);
+    QCOMPARE(PokitDiscoveryAgent::isPokitDevice(info), expected);
 }
 
 QTEST_MAIN(TestPokitDiscoveryAgent)
