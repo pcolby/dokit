04258f39ca7ab6376a01d55cfb0adcb8f8c8acce
Paul Colby <git@colby.id.au>
2022-05-02 20:18:27 +1000

    Fix pre-Qt5.13 compile issue

--- test/unit/testpokitdiscoveryagent.cpp
+++ test/unit/testpokitdiscoveryagent.cpp
@@ -25,6 +25,13 @@
 
 #define POKIT_STATUS_SERVICE_UUID QLatin1String("57d3a771-267c-4394-8872-78223e92aec5")
 
+// QBluetoothDeviceInfo::setServiceUuids deprecated the completeness argument in Qt 5.13.
+#if (QT_VERSION < QT_VERSION_CHECK(5, 13, 0))
+#define DATA_COMPLETENESS , QBluetoothDeviceInfo::DataUnavailable
+#else
+#define DATA_COMPLETENESS
+#endif
+
 void TestPokitDiscoveryAgent::isPokitDevice_data()
 {
     QTest::addColumn<QBluetoothDeviceInfo>("info");
@@ -35,19 +42,19 @@ void TestPokitDiscoveryAgent::isPokitDevice_data()
     QBluetoothDeviceInfo info(QBluetoothAddress(QLatin1String("11:22:33:44:55:66")), QLatin1String("info"), 0);
     QTest::addRow("unset") << info << false;
 
-    info.setServiceUuids({});
+    info.setServiceUuids({} DATA_COMPLETENESS);
     QTest::addRow("empty") << info << false;
 
-    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid())});
+    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid())} DATA_COMPLETENESS);
     QTest::addRow("random") << info << false;
 
-    info.setServiceUuids({ QBluetoothUuid(POKIT_STATUS_SERVICE_UUID) });
+    info.setServiceUuids({ QBluetoothUuid(POKIT_STATUS_SERVICE_UUID) } DATA_COMPLETENESS);
     QTest::addRow("status") << info << true;
 
-    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid()), QBluetoothUuid(QUuid::createUuid())});
+    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid()), QBluetoothUuid(QUuid::createUuid())} DATA_COMPLETENESS);
     QTest::addRow("two") << info << false;
 
-    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid()), QBluetoothUuid(POKIT_STATUS_SERVICE_UUID)});
+    info.setServiceUuids({QBluetoothUuid(QUuid::createUuid()), QBluetoothUuid(POKIT_STATUS_SERVICE_UUID)} DATA_COMPLETENESS);
     QTest::addRow("both") << info << true;
 }
 
