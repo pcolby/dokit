4a8342f5e5bfcacd9a293106de88165a0f2a71a3
Paul Colby <git@colby.id.au>
2022-05-24 19:59:44 +1000

    Switch to SPDX license identifiers

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2022 Paul Colby <git@colby.id.au>
+# SPDX-License-Identifier: LGPL-3.0-or-later
 
 cppcheck -DQTPOKIT_NAMESPACE=qtpockit -UQ_DECLARE_PRIVATE \
            --enable=all --error-exitcode=1 -Isrc \
--- test/unit/testinfocommand.cpp
+++ test/unit/testinfocommand.cpp
@@ -1,21 +1,5 @@
-/*
-    Copyright 2022 Paul Colby
-
-    This file is part of QtPokit.
-
-    QtPokit is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    QtPokit is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public License
-    along with QtPokit.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// SPDX-FileCopyrightText: 2022 Paul Colby <git@colby.id.au>
+// SPDX-License-Identifier: LGPL-3.0-or-later
 
 #include "testinfocommand.h"
 
