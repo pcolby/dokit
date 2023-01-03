2c7672da2a444cf06f452e2f1fbdf0f6694a451d
Paul Colby <git@colby.id.au>
2023-01-03 12:02:44 +1100

    Cppcheck more exhaustively

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -2,8 +2,18 @@
 # SPDX-FileCopyrightText: 2022-2023 Paul Colby <git@colby.id.au>
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
-cppcheck -DQTPOKIT_NAMESPACE=qtpockit -UQ_DECLARE_PRIVATE \
-           --enable=all --error-exitcode=1 -Isrc \
-           --suppress={missingInclude,unknownMacro} \
-           --suppress=unusedFunction:test/unit/*.cpp \
-           src test
+cppcheck --version
+RC=0
+for platform in native unix{32,64} win{32{A,W},64} unspecified; do
+  for defines in -{D,U}QTPOKIT_LIBRARY' '-{DQTPOKIT_NAMESPACE=qtpokit,UQTPOKIT_NAMESPACE}' '-{DPROJECT_BUILD_ID=\"1234\",UPROJECT_BUILD_ID}' '-{DPROJECT_PRE_RELEASE=pre,UPROJECT_PRE_RELEASE}; do
+    echo "$platform $defines"
+    cppcheck -UQ_DECLARE_PRIVATE $defines \
+      --enable=all --error-exitcode=1 -Isrc \
+      --language=c++ --platform="$platform" --std=c++{03,11,14,17,20} \
+      --suppress={missingInclude,unknownMacro} \
+      --suppress=unusedFunction:test/unit/*.cpp \
+      --quiet "$@" src test | grep -Eve '^Checking' | uniq --skip-fields=1
+    RC=$((RC+$?))
+  done
+done
+exit $RC
