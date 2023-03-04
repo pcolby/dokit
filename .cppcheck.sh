e269ccffc544b7d85126de84b735aca0dad7475d
Paul Colby <git@colby.id.au>
2023-03-04 16:47:06 +1100

    Fix a minor shellcheck warning

--- .cppcheck.sh
+++ .cppcheck.sh
@@ -7,7 +7,8 @@ RC=0
 for platform in native unix{32,64} win{32{A,W},64} unspecified; do
   for defines in -{D,U}QTPOKIT_LIBRARY' '-{DQTPOKIT_NAMESPACE=qtpokit,UQTPOKIT_NAMESPACE}' '-{DPROJECT_BUILD_ID=\"1234\",UPROJECT_BUILD_ID}' '-{DPROJECT_PRE_RELEASE=pre,UPROJECT_PRE_RELEASE}; do
     echo "$platform $defines"
-    cppcheck -UQ_DECLARE_PRIVATE $defines \
+    read -a definesArray -r <<< "$defines"
+    cppcheck -UQ_DECLARE_PRIVATE "${definesArray[@]}" \
       --enable=all --error-exitcode=1 -Isrc \
       --language=c++ --platform="$platform" --std=c++{03,11,14,17,20} \
       --suppress={missingInclude,unknownMacro} \
