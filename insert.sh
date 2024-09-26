3663937cbe902c4d2f117188bc8bd3510036e3fb
Paul Colby <git@colby.id.au>
2024-09-26 22:27:27 +1000

    Harden the shell script

--- insert.sh
+++ insert.sh
@@ -1,2 +1,8 @@
+#!/usr/bin/env bash
+# SPDX-FileCopyrightText: 2022-2024 Paul Colby <git@colby.id.au>
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
 find . -mindepth 2 -name cov -prune -or -name '*.html' -print |
   xargs -n 1000 -P 8 awk -f versions.awk -i '/usr/share/awk/inplace.awk' --
