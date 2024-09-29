8c3f1f1c5b2d18a249c46abf2207f2ba8a6d9ee7
Paul Colby <git@colby.id.au>
2024-09-29 10:59:04 +1000

    Add some documentation to the `.selector.sh` script

--- .selector.sh
+++ .selector.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 # SPDX-FileCopyrightText: 2022-2024 Paul Colby <git@colby.id.au>
 # SPDX-License-Identifier: LGPL-3.0-or-later
+#
+# This script is used to inject a 'version selector' into each of the documentation versions, to make it easier to
+# navigate between them. It does this by running AWK script (which does the real work) on every HTML file (except the
+# the very top level `index.html` index). The AWK script replaces the (Doxygen or LCOV) HTML pages' project version
+# a JavaScript and HTML snippet that renders select boxes for each of the published versions, with relevant version
+# pre-selected. See also: `.selector.awk` and `.selector.html`
 
 set -o errexit -o noclobber -o nounset -o pipefail
 
