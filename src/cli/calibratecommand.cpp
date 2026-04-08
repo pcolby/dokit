52e8a13a1c51249c0c7ed556c4bbaa33beea99ad
Paul Colby <git@colby.id.au>
2026-04-08 19:05:21 +1000

    Fix bug in calibration command

--- src/cli/calibratecommand.cpp
+++ src/cli/calibratecommand.cpp
@@ -91,7 +91,7 @@ void CalibrateCommand::serviceDetailsDiscovered()
     Q_ASSERT(service);
     DeviceCommand::serviceDetailsDiscovered(); // Just logs consistently.
     qCInfo(lc).noquote() << tr("Calibrating temperature at %1 degrees celsius...").arg(temperature);
-    if (!service->calibrateTemperature(0)) {
+    if (!service->calibrateTemperature(temperature)) {
         QCoreApplication::exit(EXIT_FAILURE);
     }
 }
